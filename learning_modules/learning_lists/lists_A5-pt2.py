b5ecd60a4355ed8ffe81733ca2cb962de72e7c18
Shantanu Jhaveri <sj06434@usc.edu>
2020-05-06 01:46:34 -0700

    learning - encryption and decryption methods

--- learning_modules/learning_lists/lists_A5-pt2.py
+++ learning_modules/learning_lists/lists_A5-pt2.py
@@ -2,18 +2,36 @@
 # not allowed to use random.shuffle(), random.sample(),
 import random
 
-msg = list(input("ENTER A MESSAGE YOU WOULD LIKE TO ENCRYPT: "))
+msg = list(input("ENTER A MESSAGE YOU WOULD LIKE TO ENCRYPT: ").lower())
 cypherKey = int(input("ENTER THE CAESAR SHIFT YOU WOULD LIKE FOR YOUR ENCRYPTION (1-25): "))
 
 alpha = list('abcdefghijklmnopqrstuvwxyz')
 end = alpha[0:int(cypherKey)]
 cypher = alpha[cypherKey:26]
 cypher += end
-print(cypherKey)
-print(msg)
+# print(cypher)
+# print(msg)
+encrypt = ''
+decrypt = ''
 
 for i in msg:
-    index = alpha.index(i)
-    print(index)
-    replacement = cypher.index(index)
-    print(replacement)
+    index = int(alpha.index(i))
+    # print(index)
+    i = cypher[index]
+    # print(i)
+    encrypt += i
+
+print("\nENCRYPTING MESSAGE...\n\tENCRYPTED MESSAGE: " + str(encrypt))
+list(encrypt)
+
+for i in encrypt:
+    index = int(cypher.index(i))
+    i = alpha[index]
+    decrypt += i
+
+print("\nDECRYPTING MESSAGE...\n\tDECRYPTED MESSAGE: " + str(decrypt) + "\n\tORIGINAL MESSAGE: " + ''.join(msg))
+
+if decrypt == msg:
+    print("DECRYPTION SUCCESSFUL")
+else:
+    print("DECRYPTION ERROR")
