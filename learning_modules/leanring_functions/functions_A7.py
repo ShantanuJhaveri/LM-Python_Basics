a6e6c28bdef535893ff179ce4e0a22a344aa364a
Shantanu Jhaveri <sj06434@usc.edu>
2020-05-09 23:06:04 -0700

    initialize python functions_A7.py

--- learning_modules/leanring_functions/functions_A7.py
+++ learning_modules/leanring_functions/functions_A7.py
@@ -1,1 +1,8 @@
 # functions assignment through A7
+# description:
+# Write a program that allows the user to play Rock, Paper, Scissors against the
+# computer.
+# When the program begins, you randomly choose a number from 0 to 2, which will
+# represent the computer’s choice with 0 for rock, 1 for paper, or 2 for scissors.
+# The user then enters his/her choice of 0 for rock, 1 for paper, or 2 for scissors.
+
