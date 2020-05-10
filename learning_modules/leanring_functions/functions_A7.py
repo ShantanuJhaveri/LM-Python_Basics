dfb42f6e940bd37768e0282176f45a53194786d0
Shantanu Jhaveri <sj06434@usc.edu>
2020-05-10 00:35:32 -0700

    done functions_A7.py

--- learning_modules/leanring_functions/functions_A7.py
+++ learning_modules/leanring_functions/functions_A7.py
@@ -6,9 +6,9 @@
 # represent the computer’s choice with 0 for rock, 1 for paper, or 2 for scissors.
 # The user then enters his/her choice of 0 for rock, 1 for paper, or 2 for scissors.
 import random
+
 run = True
 
-def main():
 
 def displayMenu():
     print("Welcome! Let's play rock, paper, scissors."
@@ -18,27 +18,32 @@ def displayMenu():
           "\n\tPaper covers rock"
           "\n\tIf both the choices are the same, it's a tie")
 
-def getComputerChoice():
-    return random.randrange(0,3)
 
-def playerChoice():
+def getComputerChoice():
+    return random.randrange(0, 3)
+
+
+def getPlayerChoice():
     while run:
         try:
             playerMove = int(input(""))
         except ValueError:
             print("Not Valid response. Please choose (0) for rock, (1) for paper or (2) for scissors")
             continue
-        if playerMove not in range(0,3):
+        if playerMove not in range(0, 3):
             print("Not Valid response. Please choose (0) for rock, (1) for paper or (2) for scissors")
             continue
         else:
             break
 
+
 gameCalc = {
-    0:2,
-    1:0,
-    2:1
+    0: 2,
+    1: 0,
+    2: 1
 }
+
+
 def playRound(computerChoice, playerChoice):
     if gameCalc[computerChoice] == playerChoice:
         return -1
@@ -49,6 +54,60 @@ def playRound(computerChoice, playerChoice):
 
 
 def continueGame():
-    print(run)
+    while run:
+        continueRequest = input("Do you want to continue playing (y or n)?")
+        if continueRequest in ('y', 'n'):
+            break
+        else:
+            print("not a valid input")
+    if continueRequest == 'y':
+        return True
+    else:
+        return False
 
 
+def names():
+    def convert(input):
+        if input == 0:
+            return "rock"
+        elif input == 1:
+            return "paper"
+        elif input == 2:
+            return "scissors"
+
+
+def main():
+    w = 0
+    l = 0
+    t = 0
+
+    while run:
+        displayMenu()
+
+        computerChoice = getComputerChoice()
+        playerChoice = getPlayerChoice()
+
+        print("You chose " + str(names(playerChoice)))
+        print("The computer chose " + str(names(computerChoice)))
+
+        final = playRound(computerChoice, playerChoice)
+
+        if final == 0:
+            print("Tie!")
+            t += 1
+        elif final == 1:
+            print("You won!")
+            w += 1
+        elif final == -1:
+            print("You lost!")
+            l += 1
+        if continueGame() == True:
+            continue
+
+        else:
+            print("You won ", w, " game(s).")
+            print("You lost ", l, " game(s).")
+            print("You tied ", t, "time(s).")
+            break
+
+main()
